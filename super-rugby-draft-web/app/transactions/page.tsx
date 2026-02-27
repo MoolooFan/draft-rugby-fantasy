30e89508ccad2182661a5b2ee77659a325b4f13d
Ethan Stout <ethanstout01@gmail.com>
2026-02-27 13:33:47 +0800

    deadline time change

--- super-rugby-draft-web/app/transactions/page.tsx
+++ super-rugby-draft-web/app/transactions/page.tsx
@@ -130,7 +130,7 @@ function isFixtureComplete(f: AnyFixture) {
 }
 
 function getSelectionDeadlineMs(firstKickoffMs: number) {
-  return firstKickoffMs - 0 * 60 * 60 * 1000; // 1h before first kickoff
+  return firstKickoffMs - -1 * 60 * 60 * 1000; // 1h before first kickoff
 }
 
 function getWeekFirstKickoffMs(fixtures: AnyFixture[], week: number) {
