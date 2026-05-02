659e8ff13ed67f1e2001eb98a51c4dd140e2c775
Ethan Stout <ethanstout01@gmail.com>
2026-05-02 14:47:49 +0800

    playerid fix

--- super-rugby-draft-web/lib/players/externalIdMap.ts
+++ super-rugby-draft-web/lib/players/externalIdMap.ts
@@ -428,7 +428,7 @@ export const EXTERNAL_PLAYER_ID_MAP: Record<string, number> = {
   "j.martens": 427,
   "t.robinson2": 428,
   "l.casey": 429,
-  "v.vakatawa": 1000,
+  "v.vakatawa": 430,
   "t.barnes": 431,
   "m.tupaea": 432,
   "r.wingham": 433,
@@ -448,10 +448,11 @@ export const EXTERNAL_PLAYER_ID_MAP: Record<string, number> = {
   "t.annan": 447,
   "s.ravudi": 448,
   "c.grant": 449,
-  "z.lomax": 925703,
+  "z.lomax": 450,
   "n.shearer": 451,
   "j.sexton": 452,
-  "o.mathis": 453
+  "o.mathis": 453,
+  "d.sinkinson": 454
 };
 
 function normaliseInternalId(x: string) {
